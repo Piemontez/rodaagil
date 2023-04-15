4817fedb36a1014ca0100ed8385e337688cdb164
piemontez <rafaelpiemontez@gmail.com>
2023-04-15 12:02:42 -0300

    rotacionar text

--- src/js/rodaagilchart.js
+++ src/js/rodaagilchart.js
@@ -76,28 +76,6 @@ function drawScore(cx, chartProps) {
   }
 }
 
-function drawTitles(cx, chartProps) {
-  // Cultura
-  cx.font = 0.01 * chartValues.width + "px Arial";
-  cx.fillStyle = chartProps.colors.text;
-
-  for (var key in chartProps.itens) {
-    var item = chartProps.itens[key];
-
-    var x =
-      chartValues.height *
-      0.43 *
-      Math.cos(key * chartValues.sliceAngle + chartValues.sliceAngle / 5);
-
-    var y =
-      chartValues.height *
-      0.43 *
-      Math.sin(key * chartValues.sliceAngle + chartValues.sliceAngle / 5);
-
-    cx.fillText(item.title, x + chartValues.centerX, y + chartValues.centerY);
-  }
-}
-
 function drawLines(cx, chartProps) {
   var count = 0;
   chartProps.itens.forEach(function (result) {
@@ -143,230 +121,122 @@ function drawLines(cx, chartProps) {
   });
 }
 
+function drawHeaders(cx, chartProps) {
+  // Textos do gráfico
+  cx.font = 0.03125 * chartValues.width + "px  Arial";
+  cx.fillStyle = chartProps.colors.text;
+
+  cx.fillText(
+    "Valor",
+    chartValues.width * 0.00625,
+    chartValues.height * 0.038461538461538464
+  );
+
+  cx.font = 0.015 * chartValues.width + "px  Arial";
+  cx.fillText(
+    "a Todo instante",
+    chartValues.width * 0.00825,
+    chartValues.height * 0.059661538461538464
+  );
+
+  cx.font = 0.03125 * chartValues.width + "px  Arial";
+  cx.fillText(
+    "Pessoas",
+    chartValues.width * 0.875,
+    chartValues.height * 0.038461538461538464
+  );
+  cx.font = 0.015 * chartValues.width + "px  Arial";
+  cx.fillText(
+    "sensacionais",
+    chartValues.width * 0.905,
+    chartValues.height * 0.059461538461538464
+  );
+
+  heightTitle = chartValues.height * 0.9469230769230769;
+  heightSubTitle = chartValues.height * 0.9769230769230769;
+
+  cx.font = 0.03125 * chartValues.width + "px  Arial";
+  cx.fillText("Experimente", chartValues.width * 0.82, heightTitle);
+  cx.font = 0.015 * chartValues.width + "px  Arial";
+  cx.fillText("e aprenda rápido", chartValues.width * 0.88125, heightSubTitle);
+
+  cx.font = 0.03125 * chartValues.width + "px  Arial";
+  cx.fillText("Segurança", chartValues.width * 0.00625, heightTitle);
+  cx.font = 0.015 * chartValues.width + "px  Arial";
+  cx.fillText("um pré requisito", chartValues.width * 0.00625, heightSubTitle);
+
+  // Informações do time e data
+  cx.font = 0.0125 * chartValues.width + "px Arial";
+
+  cx.fillText(
+    "Time: " + chartProps.time,
+    chartValues.width * 0.0125,
+    chartValues.height * 0.09230769230769231
+  );
+
+  cx.fillText(
+    "Data: " + chartProps.date,
+    chartValues.width * 0.0125,
+    chartValues.height * 0.11538461538461539
+  );
+}
+
+function splitText(txt) {
+  return txt.split(" ").reduce(
+    (last, curr) => {
+      const size = last[last.length - 1].length + curr.length;
+      if (size < 17) {
+        last[last.length - 1] += " " + curr;
+      } else {
+        last.push(curr);
+      }
+      return last;
+    },
+    [""]
+  );
+}
+
+function drawTitles(cx, chartProps) {
+  // Cultura
+  cx.font = 0.015 * chartValues.width + "px Arial";
+  cx.fillStyle = chartProps.colors.text;
+
+  for (var key in chartProps.itens) {
+    var item = chartProps.itens[key];
+    const titles = splitText(item.title);
+
+    for (var keyTxt in titles) {
+      const title = titles[keyTxt];
+      cx.save();
+
+      var angle = key * chartValues.sliceAngle + chartValues.sliceAngle * 0.05 + (keyTxt * 0.006);
+      var distance = chartValues.height * 0.48 - (chartValues.height * keyTxt * 0.02);
+      var x = distance * Math.cos(angle);
+      var y = distance * Math.sin(angle);
+
+      cx.translate(x + chartValues.centerX, y + chartValues.centerY);
+      cx.rotate(angle + Math.PI / 1.9);
+
+      cx.fillText(title, 0, 0);
+
+      cx.restore();
+    }
+  }
+}
+
 function draw(canvas, chartProps) {
   var cx = canvas.getContext("2d");
+  cx.save();
 
   configureChart(canvas, chartProps);
   drawBorder(cx, canvas.width, canvas.height);
   drawScore(cx, chartProps);
   drawLines(cx, chartProps);
 
-  // Textos do gráfico
-  cx.font = 0.03125 * canvas.width + "px  Arial";
-  cx.fillStyle = chartProps.colors.text;
-  cx.fillText(
-    "Valor",
-    canvas.width * 0.00625,
-    canvas.height * 0.038461538461538464
-  );
-
-  cx.font = 0.015 * canvas.width + "px  Arial";
-  cx.fillText(
-    "a Todo instante",
-    canvas.width * 0.00825,
-    canvas.height * 0.059661538461538464
-  );
-
-  cx.font = 0.03125 * canvas.width + "px  Arial";
-  cx.fillText(
-    "Pessoas",
-    canvas.width * 0.875,
-    canvas.height * 0.038461538461538464
-  );
-  cx.font = 0.015 * canvas.width + "px  Arial";
-  cx.fillText(
-    "sensacionais",
-    canvas.width * 0.905,
-    canvas.height * 0.059461538461538464
-  );
-
-  heightTitle = canvas.height * 0.9469230769230769;
-  heightSubTitle = canvas.height * 0.9769230769230769;
-
-  cx.font = 0.03125 * canvas.width + "px  Arial";
-  cx.fillText("Experimente", canvas.width * 0.82, heightTitle);
-  cx.font = 0.015 * canvas.width + "px  Arial";
-  cx.fillText("e aprenda rápido", canvas.width * 0.88125, heightSubTitle);
-
-  cx.font = 0.03125 * canvas.width + "px  Arial";
-  cx.fillText("Segurança", canvas.width * 0.00625, heightTitle);
-  cx.font = 0.015 * canvas.width + "px  Arial";
-  cx.fillText("um pré requisito", canvas.width * 0.00625, heightSubTitle);
-
+  drawHeaders(cx, chartProps);
   drawTitles(cx, chartProps);
-  /*
-  // Organização
-  cx.fillText("Práticas", canvas.width * 0.87, canvas.height * 0.561);
-  cx.fillText("Lean-Agile", canvas.width * 0.858, canvas.height * 0.584);
-
-  cx.fillText(
-    "Métricas e",
-    canvas.width * 0.84,
-    canvas.height * 0.7076923076923077
-  );
-  cx.fillText(
-    "ferramentas",
-    canvas.width * 0.838,
-    canvas.height * 0.7307692307692307
-  );
-
-  cx.fillText(
-    "Comprometimento",
-    canvas.width * 0.75,
-    canvas.height * 0.823076923076923
-  );
-  cx.fillText(
-    "com o produto",
-    canvas.width * 0.77,
-    canvas.height * 0.8461538461538461
-  );
-
-  cx.fillText(
-    "Compartilhamento",
-    canvas.width * 0.63125,
-    canvas.height * 0.9076923076923077
-  );
-  cx.fillText(
-    "de conhecimento",
-    canvas.width * 0.64325,
-    canvas.height * 0.9307692307692308
-  );
-
-  cx.fillText(
-    "Ritmo das",
-    canvas.width * 0.5225,
-    canvas.height * 0.9461538461538461
-  );
-  cx.fillText(
-    "Entregas",
-    canvas.width * 0.530625,
-    canvas.height * 0.9692307692307692
-  );
-
-  // Técnico
-  cx.fillText(
-    "Código",
-    canvas.width * 0.399375,
-    canvas.height * 0.926923076923077
-  );
-  cx.fillText("Sutentável", canvas.width * 0.394375, canvas.height * 0.95);
-  cx.fillText(
-    "(Refactoring)",
-    canvas.width * 0.388125,
-    canvas.height * 0.9730769230769231
-  );
-
-  cx.fillText(
-    "Automações",
-    canvas.width * 0.260625,
-    canvas.height * 0.9153846153846154
-  );
-  cx.fillText(
-    "(Pipeline)",
-    canvas.width * 0.273125,
-    canvas.height * 0.9384615384615385
-  );
-
-  cx.fillText(
-    "Práticas BDD/TDD",
-    canvas.width * 0.13125,
-    canvas.height * 0.8153846153846154
-  );
-  cx.fillText(
-    "e Clean Code",
-    canvas.width * 0.14375,
-    canvas.height * 0.8384615384615385
-  );
-
-  cx.fillText(
-    "Qualidade",
-    canvas.width * 0.1,
-    canvas.height * 0.6923076923076923
-  );
-  cx.fillText(
-    "E2E",
-    canvas.width * 0.11875,
-    canvas.height * 0.7153846153846154
-  );
-
-  cx.fillText(
-    "Práticas",
-    canvas.width * 0.08125,
-    canvas.height * 0.5461538461538461
-  );
-  cx.fillText(
-    "DevOps",
-    canvas.width * 0.08125,
-    canvas.height * 0.5692307692307692
-  );
-
-  // Negócio
-  cx.fillText(
-    "Quebra de Stories,",
-    canvas.width * 0.03125,
-    canvas.height * 0.43846153846153846
-  );
-  cx.fillText(
-    "Features e Epics",
-    canvas.width * 0.0375,
-    canvas.height * 0.46153846153846156
-  );
-
-  cx.fillText("User", canvas.width * 0.1, canvas.height * 0.2846153846153846);
-  cx.fillText(
-    "Experience",
-    canvas.width * 0.08125,
-    canvas.height * 0.3076923076923077
-  );
-
-  //cx.fillText("Processo de",canvas.width * 0.14375 ,canvas.height * 0.16923076923076924);
-  //cx.fillText("Criação",canvas.width * 0.159375 ,canvas.height * 0.19230769230769232);
-  cx.fillText(
-    "Entrega de",
-    canvas.width * 0.15375,
-    canvas.height * 0.14923076923076924
-  );
-  cx.fillText(
-    "Valor",
-    canvas.width * 0.169375,
-    canvas.height * 0.17230769230769232
-  );
-
-  //cx.fillText("Entrega de",canvas.width * 0.26875 ,canvas.height * 0.08076923076923077);
-  //cx.fillText("valor",canvas.width * 0.29375 ,canvas.height * 0.10384615384615385);
-  cx.fillText(
-    "Relacionamento",
-    canvas.width * 0.24875,
-    canvas.height * 0.06076923076923077
-  );
-  cx.fillText(
-    "com negócio",
-    canvas.width * 0.27375,
-    canvas.height * 0.08384615384615385
-  );
-
-  cx.fillText(
-    "Satisfação",
-    canvas.width * 0.39125,
-    canvas.height * 0.026923076923076925
-  );
-  cx.fillText("do cliente", canvas.width * 0.394375, canvas.height * 0.05);
-*/
-  // Informações do time e data
-  cx.font = 0.0125 * canvas.width + "px Arial";
-
-  cx.fillText(
-    "Time: " + chartProps.time,
-    canvas.width * 0.0125,
-    canvas.height * 0.09230769230769231
-  );
-
-  cx.fillText(
-    "Data: " + chartProps.date,
-    canvas.width * 0.0125,
-    canvas.height * 0.11538461538461539
-  );
 
   imgData = canvas.toDataURL();
+
   cx.restore();
 }
